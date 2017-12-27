be7b83f8221a6b71e99cc340061d07a51d1f263e
Sultanxda <sultanxda@gmail.com>
2017-12-27 11:20:41 -0800

    Remove support for OnePlus 5 (cheeseburger)
    This tree will only be for the OnePlus 5T (dumpling).
    
    Remove the 1080p panel configs, as well as the fpc1020 driver, as they
    won't be used on dumpling.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2339,7 +2339,7 @@ void composite_suspend(struct usb_gadget *gadget)
 	cdev->suspended = 1;
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
-#ifndef CONFIG_MACH_MSM8998_ONEPLUS5
+#ifndef CONFIG_MACH_MSM8998_DUMPLING
 	usb_gadget_vbus_draw(gadget, 2);
 #endif
 }
